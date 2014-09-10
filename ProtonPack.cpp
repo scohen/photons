ee5f5e1f36b7a1436ac1cd29adbab32f149feefb
Steve Cohen <steve@pinterest.com>
2014-09-09 21:35:07 -0700

    callbacks work

--- ProtonPack.cpp
+++ ProtonPack.cpp
@@ -1,197 +1,73 @@
 #include "Arduino.h"
-#include <tlc_fades.h>
+//#include <tlc_fades.h>
 #include <tlc_config.h>
-#include <tlc_animations.h>
-#include <tlc_progmem_utils.h>
+//#include <tlc_animations.h>
+//#include <tlc_progmem_utils.h>
 #include <Tlc5940.h>
-#include <tlc_servos.h>
-#include <tlc_shifts.h>
-
+//#include <tlc_servos.h>
+//#include <tlc_shifts.h>
+#include <typeinfo>
 #include "ProtonPack.h"
-#define INPUT_PULLUP 0x20
-
 
 int increment = 50;
 int last_debounce_time = 0;
 int direction = 1;
 
-int pp_increment_to_max(int current, int max_value) {    
+int pp_increment_to_max(int current, int max_value) {
     int incremented = current += 1;
-    if (incremented >= max_value){
+    if (incremented >= max_value) {
         return 0;
     }
     return incremented;
 }
 
-int pp_decrement(int current, int max_value) {    
+int pp_decrement(int current, int max_value) {
     int incremented = current -= 1;
-    if (incremented < 0 ){
+    if (incremented < 0 ) {
         return max_value;
     }
     return incremented;
 }
 
-void _pp_defaultUpdateNozzle(Pack* pack, Nozzle* nozzle) {
-    nozzle->led = 4000;
+void PackComponent::callAgainIn(int num_millis) {
+    _next_call_time = millis() + (long) num_millis;
 }
 
-void _pp_defaultUpdateCyclotron(Pack* pack, Cyclotron* cyclotron) {
-    static int BRIGHTNESS_INCREMENT = 83;
-    unsigned long last_updated = pack->now - cyclotron->last_updated;
-        if (last_updated >= 1000) {
-        cyclotron->current_brightness = 0;
-        cyclotron->current_led = pp_increment_to_max(cyclotron->current_led, 4);
-        cyclotron->last_updated = pack->now;
-    } else if ( last_updated < 500 ) {
-        cyclotron->current_brightness += BRIGHTNESS_INCREMENT;
-        cyclotron->leds[cyclotron->current_led] = cyclotron->current_brightness;
-        
-    } else if (cyclotron->current_brightness > 0) {
-        cyclotron->current_brightness -= BRIGHTNESS_INCREMENT;
-        cyclotron->leds[cyclotron->current_led] = cyclotron->current_brightness;
-    }
-
+void PackComponent::setLed(int ledNum, int val) {
+    Tlc.set(ledNum + _offset, val);
 }
 
-void _pp_defaultUpdatePowercell(Pack* pack, Powercell* cell) {
-    unsigned long last_updated = pack->now - cell->last_updated;
-    if (last_updated >= cell->UPDATE_RATE){
-        if (pack->is_initializing) {
-            for(int i=0; i < cell->num_leds; i++){
-                if (i == cell->current_led){
-                    cell->leds[i] = cell->MAX_BRIGHTNESS;
-                } else {
-                    cell->leds[i] = 0;
-                }
-                
-            }
-            if (cell->current_led > 0){
-                cell->leds[cell->current_led - 1] = cell->MAX_BRIGHTNESS / 2;
-            }
-                
-            if (cell->current_led > 1) {
-                cell->leds[cell->current_led] = cell->MAX_BRIGHTNESS / 4;
-            }
-            cell->current_led = pp_decrement(cell->current_led, cell->num_leds - 1);            
-            cell->current_led %= cell->num_leds;
-        } else {
-            if (cell->current_led == cell->num_leds - 1){
-                for (int i=0; i<= cell->num_leds; i++) {
-                  cell->leds[i] = 0;  
-                }
-            }
-            cell->leds[cell->current_led] = cell->MAX_BRIGHTNESS;            
-            cell->current_led = pp_decrement(cell->current_led, cell->num_leds - 1);            
-        }
-        cell->last_updated = millis();
-    }
+
+void PackComponent::reset(Pack* pack) {
+    _last_updated = 0;
+    _next_call_time = 0;
 }
 
-void _pp_defaultUpdateGraph(Pack* pack, Graph* graph){
-    
-    int O = 1000;
-    int o = 4000;
-    int _ = 0;
-    const int firing_sequence[][15] = {
-      {_, _, _, _, _, _, _, O, _, _, _, _, _, _, _},
-      {_, _, _, _, _, _, o, O, o, _, _, _, _, _, _},
-      {_, _, _, _, _, o, O, _, O, o, _, _, _, _, _},
-      {_, _, _, _, o, O, _, _, _, O, o, _, _, _, _},
-      {_, _, _, o, O, _, _, _, _, _, O, o, _, _, _},
-      {_, _, o, O, _, _, _, _, _, _, _, O, o, _, _},
-      {_, o, O, _, _, _, _, _, _, _, _, _, O, o, _},
-      {o, O, _, _, _, _, _, _, _, _, _, _, _, O, o},
-    };
-    const int normal_sequence[][15] = {
-        {o, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
-        {O, o, _, _, _, _, _, _, _, _, _, _, _, _, _},
-        {O, O, o, _, _, _, _, _, _, _, _, _, _, _, _},
-        {O, O, O, o, _, _, _, _, _, _, _, _, _, _, _},
-        {O, O, O, O, o, _, _, _, _, _, _, _, _, _, _},
-        {O, O, O, O, O, o, _, _, _, _, _, _, _, _, _},
-        {O, O, O, O, O, O, o, _, _, _, _, _, _, _, _},
-        {O, O, O, O, O, O, O, o, _, _, _, _, _, _, _},
-        {O, O, O, O, O, O, O, O, _, _, _, _, _, _, _},
-        {O, O, O, O, O, O, O, o, _, _, _, _, _, _, _},
-        {O, O, O, O, O, O, o, _, _, _, _, _, _, _, _},
-        {O, O, O, O, O, o, _, _, _, _, _, _, _, _, _},
-        {O, O, O, O, o, _, _, _, _, _, _, _, _, _, _},
-        {O, O, O, o, _, _, _, _, _, _, _, _, _, _, _},
-        {O, O, o, _, _, _, _, _, _, _, _, _, _, _, _},
-        {O, o, _, _, _, _, _, _, _, _, _, _, _, _, _},
-        {o, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
-        {_, _, _, _, _, _, _, _, _, _, _, _, _, _, _},
-    };
-    
-
-    unsigned long last_updated = pack->now - graph->last_updated;
-    
-    if (last_updated >= 40){
-        if(pack->is_firing){
-            for(int i=0; i < graph->num_leds; i++){
-                graph->leds[i] = firing_sequence[graph->iteration][i];
-            }
-            graph->iteration++;
-            graph->iteration %= 8;            
-        } else {
-            for(int i=0; i < graph->num_leds; i++){
-                graph->leds[i] = normal_sequence[graph->iteration][i];
-            }
-            graph->iteration++;
-            graph->iteration %= 18;            
-        }
-        graph->last_updated = millis(); 
-    }
-    
+PackComponent::PackComponent(int offset) {
+    _offset = offset;
 }
+
+bool PackComponent::isReadyToUpdate() {
+    return _next_call_time <= millis();
+}
+
+void PackComponent::onUpdate(Pack* pack) {
+    Serial.println("onUpdate for PackComponent");
+}
+void PackComponent::onActivateButtonPress(Pack* pack){}
+void PackComponent::onFiringStart(Pack* pack){}
+void PackComponent::onFiringStop(Pack* pack){}
+void PackComponent::onPackStartUp(Pack* pack){}
+void PackComponent::onPackShutDown(Pack* pack){}
+void PackComponent::onPackInitStart(Pack* pack){}
+void PackComponent::onPackInitComplete(Pack* pack){}
+
 
 ProtonPack::ProtonPack(int power_switch_id,
-                       int activate_switch_id,
-                       int cyclotron_offset,
-                       int powercell_offset,
-                       int graph_offset) {
+                       int activate_switch_id) {
     _power_switch_id = power_switch_id;
     _activate_switch_id = activate_switch_id;
-    _cyclotron_offset = cyclotron_offset;
-    _powercell_offset = powercell_offset;
-    _graph_offset = graph_offset;
-    setCyclotronUpdateCallback(_pp_defaultUpdateCyclotron);
-    setPowercellUpdateCallback(_pp_defaultUpdatePowercell);
-    setGraphUpdateCallback(_pp_defaultUpdateGraph);
-    setNozzleUpdateCallback(_pp_defaultUpdateNozzle);
     reset();
-}
-
-void resetNozzle(Nozzle* _nozzle) {
-    _nozzle->last_updated = 0;
-    _nozzle->led = 0;
-}
-
-void resetCyclotron(Cyclotron* _cyclotron) {
-    _cyclotron->last_updated = 0;
-    _cyclotron->current_led = -1;
-    _cyclotron->current_brightness = 0;
-    for (int i=0; i < 4; i++) {
-        _cyclotron->leds[i] = 0;
-    }
-}
-
-void resetPowercell(Powercell* _cell) {
-    _cell->last_updated = 0;
-    _cell->current_led = 0;
-    _cell->initializing = true;
-    _cell->current_brightness = 0;
-    for(int i=0; i < _cell->num_leds; i++) {
-        _cell->leds[i] = 0;
-    }
-}
-
-void resetGraph(Graph* _graph){
-    _graph->last_updated = 0;
-    _graph->iteration = 0;    
-    for(int i=0; i < _graph->num_leds; i++) {
-        _graph->leds[i] = 0;
-    }
 }
 
 void resetPack(Pack* _pack) {
@@ -199,77 +75,58 @@ void resetPack(Pack* _pack) {
     _pack->started_at = _pack->now;
     _pack->is_on = false;
     _pack->is_firing = false;
-    _pack->is_initializing = false;    
+    _pack->is_initializing = false;
 }
 
 void ProtonPack::reset() {
     Tlc.clear();
-    resetPowercell(&_cell);
-    resetCyclotron(&_cyclotron);
-    resetPack(&_pack);
-    resetGraph(&_graph);
-    resetNozzle(&_nozzle);
-    _pack.powercell = _cell;
-    _pack.cyclotron = _cyclotron;
-    _pack.graph = _graph;
-    _pack.nozzle = _nozzle;
+    for(int i=0; i < _components.size(); i++) {
+        _components[i]->reset(&_pack);
+    }
 }
 
 void ProtonPack::initialize() {
     Tlc.init();
     pinMode(_power_switch_id, INPUT);
-    
-    
+
+
     _power_button_state = 0;
     _activate_button_state = 0;
 }
 
-void ProtonPack::_updatePowercell() {
-    _update_powercell_cb(&_pack, &_cell);
-    for(int i=0; i < _cell.num_leds; i++) {
-        Tlc.set(_powercell_offset + i, _cell.leds[i]);
-    }
-}
-
-void ProtonPack::_updateCyclotron() {
-    _update_cyclotron_cb(&_pack, &_cyclotron);
-    for(int i=0; i < 4; i++) {
-        Tlc.set(_cyclotron_offset + i, _cyclotron.leds[i]);
-    }
-}
-
-void ProtonPack::_updateGraph(){
-    _update_graph_cb(&_pack, &_graph);
-    for(int i=0; i < 48; i++){
-        Tlc.set(_graph_offset + i, _graph.leds[i]);        
-    }
-}
-
-void ProtonPack::_updateNozzle() {
-    _update_nozzle_cb(&_pack, &_nozzle);
-    Tlc.set(_graph_offset + 16, _nozzle.led);
+void ProtonPack::addComponent(PackComponent *component) {
+    _components.push_back(component);
 }
 
 void ProtonPack::update() {
     _pack.now = millis();
-    
+
     int latestPowerButtonState = digitalRead(_power_switch_id);;
     int latestActivateButtonState = digitalRead(_activate_switch_id);;
 
     boolean shutting_down = false;
     boolean starting_up = false;
     boolean is_initializing_now = (_pack.now - _pack.started_at) < 5000;
-    if(is_initializing_now and ! _pack.is_initializing){
-        resetPowercell(&_cell);
-        
-    } else if (_pack.is_initializing && !is_initializing_now){
-        resetPowercell(&_cell);
+
+    if (is_initializing_now && ! _pack.is_initializing) {
+        for(int i=0; i < _components.size(); i++) {
+            PackComponent *c = _components[i];
+            Serial.println("Pack init start");
+            c->onPackInitStart(&_pack);
+        }
+    } else if (!is_initializing_now && _pack.is_initializing) {
+        for(int i=0; i < _components.size(); i++) {
+            PackComponent *c = _components[i];
+            Serial.println("Pack init stop");
+            c->onPackInitComplete(&_pack);
+        }
     }
-    
+
     _pack.is_initializing = is_initializing_now;
 
     if (millis() - last_debounce_time > 300) {
-        if ( latestPowerButtonState != _power_button_state){
+        if ( latestPowerButtonState != _power_button_state) {
+            Serial.println("Switch detected");
             if(_pack.is_on) {
                 shutting_down = true;
             } else {
@@ -277,11 +134,11 @@ void ProtonPack::update() {
             }
             _pack.is_on = !_pack.is_on;
             last_debounce_time = millis();
-            _pack.now = last_debounce_time;                    
+            _pack.now = last_debounce_time;
             _power_button_state = latestPowerButtonState;
         }
-        if (_pack.is_on){
-            if (latestActivateButtonState != _activate_button_state){            
+        if (_pack.is_on) {
+            if (latestActivateButtonState != _activate_button_state) {
                 _pack.is_firing = !_pack.is_firing;
                 _activate_button_state = latestActivateButtonState;
                 last_debounce_time = millis();
@@ -291,39 +148,28 @@ void ProtonPack::update() {
 
     if (starting_up) {
         _pack.started_at = _pack.now;
-        _cell.last_updated = _pack.now;
-        _cyclotron.last_updated = _pack.now;
-        
+        for(int i=0; i < _components.size(); i++) {
+            Serial.println("Calling onPackStartUp");
+            PackComponent *c = _components[i];
+            c->onPackStartUp(&_pack);
+        }
+
     } else if (shutting_down) {
-        resetPack(&_pack);
-        resetCyclotron(&_cyclotron);
-        resetPowercell(&_cell);
+        for(int i=0; i < _components.size(); i++) {
+            Serial.println("Calling onPackShutDown");
+            PackComponent *c = _components[i];
+            c->onPackShutDown(&_pack);
+        }
         Tlc.clear();
-        Tlc.update();
     }
 
     if (_pack.is_on) {
-        _updatePowercell();
-        _updateCyclotron();
-        _updateGraph();
-        _updateNozzle();
-        Tlc.update();
+        for(int i=0; i < _components.size(); i++) {
+            PackComponent *c = _components[i];
+            if (c->isReadyToUpdate()) {
+                c->onUpdate(&_pack);
+            }
+        }
     }
-
-}
-
-void ProtonPack::setPowercellUpdateCallback(updatePowercellCallback cb) {
-    _update_powercell_cb = cb;
-}
-
-void ProtonPack::setCyclotronUpdateCallback(updateCyclotronCallback cb) {
-    _update_cyclotron_cb = cb;
-}
-
-void ProtonPack::setGraphUpdateCallback(updateGraphCallback cb) {
-    _update_graph_cb = cb;
-}
-
-void ProtonPack::setNozzleUpdateCallback(updateNozzleCallback cb) {
-    _update_nozzle_cb = cb;
+    Tlc.update();
 }
--- ProtonPack.h
+++ ProtonPack.h
@@ -4,93 +4,110 @@
 
 #include "Arduino.h"
 
-typedef struct _powercell {    
-    const static int num_leds = 14;
-    const static int UPDATE_RATE = 30;
-    const static int MAX_BRIGHTNESS = 2000;
-    int leds[num_leds];
-    int offset;
-    unsigned long last_updated;
-    int current_led;
-    int current_brightness;
-    boolean initializing;
+int pp_decrement(int current, int max_value);
+int pp_increment_to_max(int current, int max_value);
 
-} Powercell;
+template<typename Data>
+class Vector {
+    size_t d_size; // Stores no. of actually stored objects
+    size_t d_capacity; // Stores allocated capacity
+    Data *d_data; // Stores data
+public:
+    Vector() : d_size(0), d_capacity(0), d_data(0) {
+    }; // Default constructor
+    Vector(Vector const &other) : d_size(other.d_size), d_capacity(other.d_capacity), d_data(0) {
+        d_data = (Data *) malloc(d_capacity*sizeof(Data));
+        memcpy(d_data, other.d_data, d_size*sizeof(Data));
+    }; // Copy constuctor
+    ~Vector() {
+        free(d_data);
+    }; // Destructor
+    Vector &operator=(Vector const &other) {
+        free(d_data);
+        d_size = other.d_size;
+        d_capacity = other.d_capacity;
+        d_data = (Data *) malloc(d_capacity*sizeof(Data));
+        memcpy(d_data, other.d_data, d_size*sizeof(Data));
+        return *this;
+    }; // Needed for memory management
+    void push_back(Data const &x) {
+        if (d_capacity == d_size) resize();
+        d_data[d_size++] = x;
+    }; // Adds new value. If needed, allocates more space
+    size_t size() const {
+        return d_size;
+    }; // Size getter
+    Data const &operator[](size_t idx) const {
+        return d_data[idx];
+    }; // Const getter
+    Data &operator[](size_t idx) {
+        return d_data[idx];
+    }; // Changeable getter
+private:
+    void resize() {
+        d_capacity = d_capacity ? d_capacity*2 : 1;
+        Data *newdata = (Data *)malloc(d_capacity*sizeof(Data));
+        memcpy(newdata, d_data, d_size * sizeof(Data));
+        free(d_data);
+        d_data = newdata;
+    };// Allocates double the old space
+};
 
-typedef struct _cyclotron {
-    int offset;
-    int leds[4];
-    unsigned long last_updated;
-    int current_led;
-    int current_brightness;
-} Cyclotron;
 
-typedef struct _graph {
-    const static int num_leds = 15;
-    unsigned long last_updated;    
-    int leds[num_leds];
-    int iteration;
-}Graph;
-
-typedef struct _nozzle {
-    unsigned long last_updated;
-    int led;
-} Nozzle;
-
-typedef struct _protonpack { 
+typedef struct _protonpack {
     int last_updated;
+    unsigned long next_call_time;
     unsigned long now;
-    unsigned long started_at;    
+    unsigned long started_at;
     boolean is_on;
-    boolean is_initializing;    
+    boolean is_initializing;
     boolean is_firing;
-    Powercell powercell;
-    Cyclotron cyclotron;
-    Graph graph;
-    Nozzle nozzle;
 } Pack;
 
-typedef void (*updatePowercellCallback)(Pack*, Powercell*);
-typedef void (*updateCyclotronCallback)(Pack*, Cyclotron*);
-typedef void (*updateGraphCallback)(Pack*, Graph*);
-typedef void (*updateNozzleCallback)(Pack*, Nozzle*);
+
+class PackComponent {
+public:    
+    PackComponent(int offset);
+    int _offset;
+    unsigned long _last_updated;
+    unsigned long _started_at;
+    unsigned long _next_call_time;
+    void callAgainIn(int num_millis);
+    bool isReadyToUpdate();
+    virtual void onUpdate(Pack* pack);
+    virtual void onActivateButtonPress(Pack* pack);
+    virtual void onFiringStart(Pack* pack);
+    virtual void onFiringStop(Pack* pack);
+    virtual void onPackStartUp(Pack* pack);
+    virtual void onPackShutDown(Pack* pack);
+    virtual void onPackInitStart(Pack* pack);
+    virtual void onPackInitComplete(Pack* pack);
+    virtual void reset(Pack* pack);
+
+protected:
+    int offset;
+    void setLed(int ledNumber, int value);
+
+};
 
 class ProtonPack {
 public:
     ProtonPack(int power_switch_id,
-               int activate_switch_id,
-               int cyclotron_offset,
-               int powercell_offset,
-               int graph_offset);
+               int activate_switch_id);
     void update();
     void initialize();
     void reset();
-    void setPowercellUpdateCallback(updatePowercellCallback);
-    void setCyclotronUpdateCallback(updateCyclotronCallback);
-    void setGraphUpdateCallback(updateGraphCallback);
-    void setNozzleUpdateCallback(updateNozzleCallback);
-    
-private:
+    void addComponent(PackComponent *component);
+
+protected:
     Pack _pack;
-    Powercell _cell;
-    Cyclotron _cyclotron;
-    Graph _graph;
-    Nozzle _nozzle;
-    updatePowercellCallback _update_powercell_cb;
-    updateCyclotronCallback _update_cyclotron_cb;
-    updateGraphCallback _update_graph_cb;
-    updateNozzleCallback _update_nozzle_cb;
-    int _cyclotron_offset;
-    int _powercell_offset;
+    Vector<PackComponent*> _components;
     int _power_switch_id;
     int _activate_switch_id;
-    int _graph_offset;
     int _power_button_state;
     int _activate_button_state;
-    void _updatePowercell();
-    void _updateCyclotron();
-    void _updateGraph();
-    void _updateNozzle();
+
+
 };
 
 #endif
